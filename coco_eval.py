47259ac8ebac164e40285dfae770914b25b7eb86
Utsav Singla <utsav18321@iiitd.ac.in>
2020-05-02 04:21:09 +0530

    hello

--- coco_eval.py
+++ coco_eval.py
@@ -2,12 +2,10 @@
 
 """
 COCO-Style Evaluations
-
 put images here datasets/your_project_name/annotations/val_set_name/*.jpg
 put annotations here datasets/your_project_name/annotations/instances_{val_set_name}.json
 put weights here /path/to/your/weights/*.pth
 change compound_coef
-
 """
 
 import json
